26ec8a2717842d256488d0794d1cf2f2379dd420
768851314@qq.com <768851314@qq.com>
2019-05-27 17:59:32 +0800

    cmt

--- src/class/map.js
+++ src/class/map.js
@@ -5,6 +5,7 @@ import * as d3 from 'd3-geo'
 
 export default class GeoMap {
     constructor() {
+        this.cameraPosition = {x: 100, y: 0, z: 100} // 相机位置
         this.scene = null; // 场景
         this.camera = null; // 相机
         this.renderer = null; // 渲染器
@@ -17,7 +18,7 @@ export default class GeoMap {
 
     init() {
         this.setScene();
-        this.setCamera({x: 0, y: 0, z: 100});
+        this.setCamera(this.cameraPosition);
         this.setRenderer();
         this.setControl();
         this.setAxes();
@@ -29,11 +30,11 @@ export default class GeoMap {
      * */
 
     getMap() {
-        let that = this;
-        axios.get('/geojson/100000.json').then(function (res) {
+        const that = this;
+        axios.get('/geojson/51.json').then(function (res) {
             if (res.status === 200) {
                 const data = res.data;
-                that.drawMap(data)
+                that.setMapData(data)
             }
         })
     }
@@ -42,40 +43,61 @@ export default class GeoMap {
      * @desc 绘制地图
      * */
 
-    drawMap(data) {
-        let that = this;
+    setMapData(data) {
+        const that = this;
+        var vector3json = [];
+        data.features.forEach(function (features) {
+            const area = features.geometry.coordinates[0]
+            area.forEach(function (points) {
+                vector3json.push(that.lnglatToVector3(points))
+            })
+        })
+        for (var i = 0; i < vector3json.length; i++) {
+            try {
+                vector3json[i] = new THREE.Vector3(vector3json[i][0], vector3json[i][1], vector3json[i][2]).multiplyScalar(0.5);
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        this.drewMap(vector3json)
+    }
 
-
-        let group = new THREE.Group();
+    /**
+     * @desc 绘制图形
+     * @param
+     * */
+    drewMap(data) {
+        const group = new THREE.Group();
         group.position.y = 0;
         this.scene.add(group);
-        var californiaPts = [];
 
+        let shape = new THREE.Shape(data);
+        let extrudeSettings = {
+            depth: 1,
+            bevelEnabled: true,
+        };
 
-        // let shape = [];
-        data.features.forEach(function (area, i) {
-            if (i === 0) {
-                area.geometry.coordinates.forEach(function (block, index) {
-                    if (index === 0) {
-                        block.forEach(function (cd, l) {
-                            californiaPts.push(new THREE.Vector2(cd[0].toFixed(2), cd[1].toFixed(2)))
-                            if (l === block.length) {
-                                californiaPts.push(new THREE.Vector2(block[0][0].toFixed(2), block[0][1].toFixed(2)))
-                            }
-                        })
-                    }
-                })
-            }
-        })
-        console.log(californiaPts)
-        for (var i = 0; i < californiaPts.length; i++) californiaPts[i].multiplyScalar(0.000001);
-        var californiaShape = new THREE.Shape(californiaPts);
+        let geometry = new THREE.ExtrudeBufferGeometry(shape, extrudeSettings);
+        let material = new THREE.MeshPhongMaterial({
+            color: 0x156289,
+            emissive: 0x072534,
+            flatShading: true
+        });
+        let mesh = new THREE.Mesh(geometry, material);
+        this.scene.add(mesh);
+    }
 
-        var geometry = new THREE.ShapeBufferGeometry(californiaShape);
-        var mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({side: THREE.DoubleSide}));
-        mesh.position.set(0, 0, 0);
-        mesh.rotation.set(0, 0, 0);
-        group.add(mesh);
+    /**
+     * @desc 坐标转换
+     * @param lnglat[x,y]
+     * */
+    lnglatToVector3(lnglat) {
+        if (!this.projection) {
+            this.projection = d3.geoMercator().center([104.072259, 30.663403]).scale(100).translate([0, 0]);
+        }
+        const [x, y] = this.projection([lnglat[0], lnglat[1]])
+        const z = 0;
+        return [y, x, z]
     }
 
     /**
@@ -99,13 +121,14 @@ export default class GeoMap {
      * @desc 创建相机
      * */
     setCamera(data) {
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+        this.camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 1, 1000);
+        this.camera.up.x = 0
+        this.camera.up.y = 0
         this.camera.up.z = 1
         const {x, y, z} = data;
         this.camera.position.set(x, y, z);
         this.camera.lookAt(0, 0, 0);
         this.scene.add(this.camera);
-        // this.setCameraHelper();
         var light = new THREE.PointLight(0xffffff, 0.8);
         this.camera.add(light);
     }
@@ -132,14 +155,15 @@ export default class GeoMap {
      * @desc 创建控制器
      * */
     setControl() {
-        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+        this.controls = new THREE.OrbitControls(this.camera);
+        this.camera.position.set(this.cameraPosition.x, this.cameraPosition.y, this.cameraPosition.z);
     }
 
     /**
      * @desc 创建一个xyz坐标轴
      * */
     setAxes() {
-        const axes = new THREE.AxesHelper(10);
+        const axes = new THREE.AxesHelper(100);
         this.scene.add(axes);
     }
 
